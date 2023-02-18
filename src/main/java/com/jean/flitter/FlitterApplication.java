0da98dcfc2f4fd917c6ae66e5369e47220cfb3bd
Bao Duong <bao.duong@revature.com>
2023-02-18 16:33:05 -0500

    template

---
+++ src/main/java/com/jean/flitter/FlitterApplication.java
@@ -0,0 +1,13 @@
+package com.jean.flitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jean/flitter/FlitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jean.flitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
