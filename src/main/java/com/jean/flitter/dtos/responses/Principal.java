acc6ab526b5165e33576c3687ac21a0c3e432c1b
Bao Duong <bao.duong@revature.com>
2023-02-19 20:42:04 -0500

    added java docs for login-feature

--- src/main/java/com/jean/flitter/dtos/responses/Principal.java
+++ src/main/java/com/jean/flitter/dtos/responses/Principal.java
@@ -3,6 +3,10 @@ package com.jean.flitter.dtos.responses;
 import com.jean.flitter.entities.User;
 import lombok.ToString;
 
+/**
+ * A class representing the authenticated user. Contains the user's ID,
+ * username, role, and authentication token.
+ */
 @ToString
 public class Principal {
   private String id;
@@ -10,17 +14,44 @@ public class Principal {
   private String role;
   private String token;
 
+  /**
+   * Default constructor for the Principal class.
+   */
   public Principal() {}
+
+  /**
+   * Constructor for the Principal class that takes a User object.
+   *
+   * @param user the User object to create the Principal from.
+   */
   public Principal(User user) {
     this.id = user.getId();
     this.username = user.getUsername();
     this.role = user.getRole().getUserRole();
   }
+
+  /**
+   * Constructor for the Principal class that takes an ID, username, and role.
+   *
+   * @param id the ID of the user.
+   * @param username the username of the user.
+   * @param role the role of the user.
+   */
   public Principal(String id, String username, String role) {
     this.id = id;
     this.username = username;
     this.role = role;
   }
+
+  /**
+   * Constructor for the Principal class that takes an ID, username, role, and
+   * authentication token.
+   *
+   * @param id the ID of the user.
+   * @param username the username of the user.
+   * @param role the role of the user.
+   * @param token the authentication token of the user.
+   */
   public Principal(String id, String username, String role, String token) {
     this.id = id;
     this.username = username;
@@ -28,12 +59,59 @@ public class Principal {
     this.token = token;
   }
 
+  /**
+   * Gets the ID of the user.
+   *
+   * @return the ID of the user.
+   */
   public String getId() { return id; }
+
+  /**
+   * Sets the ID of the user.
+   *
+   * @param id the ID of the user.
+   */
   public void setId(String id) { this.id = id; }
+
+  /**
+   * Gets the username of the user.
+   *
+   * @return the username of the user.
+   */
   public String getUsername() { return username; }
+
+  /**
+   * Sets the username of the user.
+   *
+   * @param username the username of the user.
+   */
   public void setUsername(String username) { this.username = username; }
+
+  /**
+   * Gets the role of the user.
+   *
+   * @return the role of the user.
+   */
   public String getRole() { return role; }
+
+  /**
+   * Sets the role of the user.
+   *
+   * @param role the role of the user.
+   */
   public void setRole(String role) { this.role = role; }
+
+  /**
+   * Gets the authentication token of the user.
+   *
+   * @return the authentication token of the user.
+   */
   public String getToken() { return token; }
+
+  /**
+   * Sets the authentication token of the user.
+   *
+   * @param token the authentication token of the user.
+   */
   public void setToken(String token) { this.token = token; }
 }
--- src/main/java/com/jean/flitter/services/TokenService.java
+++ src/main/java/com/jean/flitter/services/TokenService.java
@@ -8,12 +8,24 @@ import io.jsonwebtoken.Jwts;
 import java.util.Date;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service responsible for generating and validating JSON Web Tokens (JWTs).
+ */
 @Service
 public class TokenService {
   private final JwtConfig jwtConfig;
 
+  /**
+   * Constructs a new TokenService object with the given JwtConfig object.
+   * @param jwtConfig the JWT configuration to use when generating tokens.
+   */
   public TokenService(JwtConfig jwtConfig) { this.jwtConfig = jwtConfig; }
 
+  /**
+   * Generates a new JWT for the given subject (user).
+   * @param subject the user to generate a token for.
+   * @return the generated JWT as a String.
+   */
   public String generateToken(Principal subject) {
     long now = System.currentTimeMillis();
     JwtBuilder tokenBuilder =
@@ -28,6 +40,12 @@ public class TokenService {
     return tokenBuilder.compact();
   }
 
+  /**
+   * Extracts the user details from the given JWT token.
+   * @param token the JWT token to extract the user details from.
+   * @return a Principal object containing the user details, or null if the
+   *     token is invalid.
+   */
   public Principal extractRequesterDetails(String token) {
     try {
       Claims claims = Jwts.parser()
